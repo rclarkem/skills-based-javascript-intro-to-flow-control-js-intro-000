c3f62c6b936ebd11470b87980f2dd0ccbb1df680
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-03-30 03:02:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-  if(age > 13 && age < 19){
+  if(age => 13 && age <= 19){
     return "You are a teenager!";
   } else {
     return undefined
@@ -7,7 +7,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age > 13 && age <= 19){
+  if(age => 13 && age <= 19){
     return "You are a teenager!";
   } else {
     return "You are not a teenager"
@@ -15,7 +15,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if(age > 13 && age < 19){
+  if(age => 13 && age <= 19){
     return "You are a teenager!";
   } else if(age <= 12){
     return "You are a kid"
