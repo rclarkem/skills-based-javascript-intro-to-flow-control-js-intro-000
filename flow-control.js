c90e5616c980f86cc526ac7a94356bb1499a1cab
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-03-30 03:16:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,5 +29,17 @@ return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    console.log("You are a teenager")
+      break;
+    default:
+    console.log("You have an age")
+  }
 }
